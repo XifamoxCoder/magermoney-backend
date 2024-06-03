e7d0ff9de77ca1e4df303cd9224231dea23e44ca
Павел Магер <pavelmager@Pavels-MacBook-Pro.local>
2024-06-03 20:21:24 +0300

    Added config module

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
-import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestApplication, NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+
+  const port = config.get<number>('port');
+  const host = config.get<string>('host');
+
+  await app.listen(port, host);
+
+  Logger.log(
+    `Application is running on http://${host}:${port}`,
+    NestApplication.name,
+  );
 }
 bootstrap();
