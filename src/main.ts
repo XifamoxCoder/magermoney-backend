9bca70df68dd636b35387c5b1916e455a43c6d92
Павел Магер <pavelmager@Pavels-MacBook-Pro.local>
2024-06-03 20:52:45 +0300

    Added swager

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestApplication, NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { certificateConfig } from '../secrets/certificates';
 import { AppModule } from './app.module';
@@ -11,9 +12,24 @@ async function bootstrap() {
     httpsOptions: { ...certificateConfig },
   });
   const config = app.get(ConfigService);
+  const apiGlobalPrefix = `${config.get<string>('apiPrefix')}/${config.get<string>('apiVersion')}`;
 
   const port = config.get<number>('port');
   const host = config.get<string>('host');
+
+  app.enableCors();
+  app.setGlobalPrefix(apiGlobalPrefix);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Magermoney API')
+    .setDescription('Magermoney API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  const swaggerPath = `${apiGlobalPrefix}/${config.get<string>('apiDocsPath')}`;
+
+  SwaggerModule.setup(swaggerPath, app, swaggerDocument);
 
   await app.listen(port, host);
 
